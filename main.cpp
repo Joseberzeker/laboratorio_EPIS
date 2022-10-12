02cb3796add034f49f7aa7fe13dddfc19836984e
Jose Luis Ahumada Navarro <63168250+Joseberzeker@users.noreply.github.com>
2022-10-12 03:49:48 -0500

    Algoritmo Actualizado

--- main.cpp
+++ main.cpp
@@ -2,91 +2,125 @@
 
 using namespace std;
 
+int salario = 0;
+int categoria = 0;
+int horasTrabajadas = 0;
+int tarifa = 0;
+
+void calcularSalario(int categoria, int horasTrabajadas )
+{
+
+	if(categoria == 1)
+	{
+
+		if(horasTrabajadas < 24)
+		{
+
+			tarifa = 12000 * 0.05;
+			salario = tarifa * horasTrabajadas;
+
+		}
+		else if( horasTrabajadas >= 24 && horasTrabajadas <= 48)
+		{
+
+			tarifa = 12000 * 0.10;
+			salario = tarifa * horasTrabajadas;
+
+		}
+		else if( horasTrabajadas > 48)
+		{
+
+			tarifa = 12000 * 0.20;
+			salario = tarifa * horasTrabajadas;
+
+		}
+
+	}
+	else if(categoria == 2)
+	{
+
+		if(horasTrabajadas < 24)
+		{
+
+			tarifa = 17000 * 0.05;
+			salario = tarifa * horasTrabajadas;
+
+		}
+		else if( horasTrabajadas >= 24 && horasTrabajadas <= 48)
+		{
+
+			tarifa = 17000 * 0.10;
+			salario = tarifa * horasTrabajadas;
+
+
+		}
+		else if( horasTrabajadas > 48)
+		{
+
+			tarifa = 17000 * 0.20;
+			salario = tarifa * horasTrabajadas;
+
+		}
+
+	}
+	else if(categoria == 3)
+	{
+
+		if(horasTrabajadas < 24)
+		{
+
+			tarifa = 22000 * 0.05;
+			salario = tarifa * horasTrabajadas;
+
+		}
+		else if( horasTrabajadas >= 24 && horasTrabajadas <= 48)
+		{
+
+			tarifa = 22000 * 0.10;
+			salario = tarifa * horasTrabajadas;
+
+		}
+		else if( horasTrabajadas > 48)
+		{
+
+			tarifa = 22000 * 0.20;
+			salario = tarifa * horasTrabajadas;
+
+		}
+
+	}
+
+	cout << "El salario es: " << salario << " pesos";
+}
+
+
+
 int main()
 {
-	int salario = 0;
-	int categoria = 0;
-	int horas_Trabajadas = 0;
-	int tarifa = 0;
-	float porcetntajeTarifa = 0;
+
+	cout << "Ingrese la categoria: ";
+	cin >> categoria;
 	
-    cout << "Ingrese la categoria: ";
-    cin >> categoria;
-    cout << "Ingrese las horas trabajadas: ";
-    cin >> horas_Trabajadas;
-    
-    if(categoria == 1){
-		
-		if(horas_Trabajadas < 24){
-			
-			tarifa = 12000 * 0.05;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";
-			
-		}else if( horas_Trabajadas >= 24 && horas_Trabajadas <= 48){
-			
-			tarifa = 12000 * 0.10;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";	
-					
-		}else if( horas_Trabajadas > 48){
-			
-			tarifa = 12000 * 0.20;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";			
-		}
-		
-	}else if(categoria == 2){
-		
-		if(horas_Trabajadas < 24){
-			
-			tarifa = 17000 * 0.05;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";
-			
-		}else if( horas_Trabajadas >= 24 && horas_Trabajadas <= 48){
-			
-			tarifa = 17000 * 0.10;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";	
-					
-		}else if( horas_Trabajadas > 48){
-			
-			tarifa = 17000 * 0.20;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";			
-		}
-		
-	}else if(categoria == 3){
-		
-		if(horas_Trabajadas < 24){
-			
-			tarifa = 22000 * 0.05;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";
-			
-		}else if( horas_Trabajadas >= 24 && horas_Trabajadas <= 48){
-			
-			tarifa = 22000 * 0.10;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";	
-					
-		}else if( horas_Trabajadas > 48){
-			
-			tarifa = 22000 * 0.20;
-			salario = tarifa * horas_Trabajadas;
-			
-			cout << "El salario es: "<< salario <<" pesos";			
-		}
+	
+	if(categoria != 1 && categoria != 2 && categoria != 3)
+	{
+		cout << "Digite una categoria valida";
 	}
-	
-    return 0;       
+	else
+	{
+		cout << "Ingrese las horas trabajadas: ";
+		cin >> horasTrabajadas;
+		
+		if(horasTrabajadas < 0)
+		{
+			cout << "Ingrese una hora valida: ";
+		
+		}
+		else
+		{
+			calcularSalario(categoria, horasTrabajadas);
+		}
+
+	}
+
 }
